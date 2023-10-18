c37ea69e06896fa4a5ce340a78d872c4cda91f5f
Onyxcodeotto <110540920+Onyxcodeotto@users.noreply.github.com>
2023-10-18 15:16:56 +0700

    feat: Pseudomap set by Coordinate

--- src/PseudoMap.java
+++ src/PseudoMap.java
@@ -8,6 +8,7 @@ public class PseudoMap {
     public void set(int i, int j, char Piece){
         this.map[i][j] = Piece;
     }
+    public void set(Coordinate coor, char Piece){this.map[coor.getX()][coor.getY()] = Piece; }
     public char get(int i, int j){
         return this.map[i][j];
     }
