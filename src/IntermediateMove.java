0d13f3ef0a3025a21aaa7f53e693cc6f77ecac19
Onyxcodeotto <110540920+Onyxcodeotto@users.noreply.github.com>
2023-10-18 11:48:38 +0700

    feat: IntermediateMove

---
+++ src/IntermediateMove.java
@@ -0,0 +1,28 @@
+public class IntermediateMove {
+     char up;
+     char left;
+     char right;
+     char bottom;
+     char center;
+
+    public IntermediateMove(){
+
+    };
+
+    public void setUp(char up){
+        this.up = up;
+    }
+    public void setRight(char right){
+        this.right = right;
+    }
+    public void setLeft(char left){
+        this.left = left;
+    }
+    public void setBottom(char bottom){
+        this.bottom = bottom;
+    }
+    public void setCenter(char center){
+        this.center = center;
+    }
+
+}
